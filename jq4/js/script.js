661057aabac9f4faacc42076fc9bfda671a88242
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-08-16 23:11:27 +0500

    Выполнили разметку для приложения

--- jq4/js/script.js
+++
@@ -1,9 +0,0 @@
-jQuery('#fader img:gt(0)').hide();
-setInterval(function(){
-    jQuery('#fader :first-child')
-        .fadeTo(500, 0)
-        .next('img')
-        .fadeTo(500, 1)
-        .end()
-        .appendTo('#fader');
-}, 4000);
