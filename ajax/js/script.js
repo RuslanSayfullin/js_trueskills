59bf15caafaf1530b74dfaf79529ee6772208e58
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-08-17 16:50:53 +0500

    Добавили js скрипты для валидации полей

---
+++ ajax/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#done").click(function() {
+        $("#messageShow").hide();
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var subject = $("#subject").val();
+        var message = $("#message").val();
+        var fail ="";
+        if(name.lenght < 3) 
+            fail = "Имя меньше 3x символов";
+        else if(email.split('@').lenght-1==0 || email.split('.').length-1==0)
+            fail = "Вы ввели неккорeктный email";
+        else if (subject.lenght <5)   
+            fail = "Тема сообщения менее 5 символов";
+        else if (message.lenght <20)   
+            fail = "Сообщение не менее 20 символов"; 
+        
+        if (fail !="") {
+            $('#messageShow').html(fail + "<div class='clear'<br></div>");
+            $('#messageShow').show();
+            return false;
+        } 
+    });
+});
